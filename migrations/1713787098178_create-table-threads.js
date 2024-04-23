33e011c86e1415104b524e4b46c60d72c99626a7
mueiya <mueiya.whatever@gmail.com>
2024-04-23 16:21:09 +0700

    fix: change the order of table colum

--- migrations/1713787098178_create-table-threads.js
+++ migrations/1713787098178_create-table-threads.js
@@ -1,32 +1,33 @@
 /* eslint-disable camelcase */
 exports.up = (pgm) => {
-    pgm.createTable('threads', {
-      id: {
-        type: 'VARCHAR(50)',
-        primaryKey: true,
-      },
-      title: {
-        type: 'TEXT',
-        notNull: true,
-      },
-      body: {
-        type: 'TEXT',
-        notNull: true,
-      },
-      date: {
-        type: 'VARCHAR(50)',
-        notNull: true,
-      },
-      owner: {
-        type: 'VARCHAR(50)',
-        notNull: true,
-      },
-    });
-  
-    // Adding foreign key users(id) as owner
-    pgm.addConstraint('threads', 'fk_threads_users', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
-  };
-  
-  exports.down = (pgm) => {
-    pgm.dropTable('threads');
-  };
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  // Adding foreign key users(id) as owner
+  pgm.addConstraint('threads', 'fk_threads_users', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
