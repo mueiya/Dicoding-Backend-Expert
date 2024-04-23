1a5abe3e794ae0c398e87da69de5162248b3f889
mueiya <mueiya.whatever@gmail.com>
2024-04-23 21:42:04 +0700

    FIx: update /POST threads handler

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,3 +1,5 @@
+const PostThreadUseCase = require("../../../../Applications/use_case/PostThreadUseCase");
+
 class ThreadsHandler {
     constructor(container) {
         this._container = container;
@@ -6,11 +8,21 @@ class ThreadsHandler {
     }
 
     async postThreadHandler(request, h) {
+        const payload = request.payload;
+        const {id: userId} = request.auth.credentials;
+        const useCasePayload = {
+          title: payload.title,
+          body: payload.body,
+          owner: userId,
+        };
+
+        const postThreadUseCase = this._container.getInstance(PostThreadUseCase.name);
+        const postedThread = await postThreadUseCase.execute(useCasePayload);
         const response = h.response({
             status: 'success',
             message: 'Thread posted successfully',
             data: {
-                addedThread,
+                addedThread: postedThread,
             }
         })
         response.code(201);
