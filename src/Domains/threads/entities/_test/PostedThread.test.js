587e929c2378cb2c75eb1343ed0ce272c60ea4cf
mueiya <mueiya.whatever@gmail.com>
2024-04-23 21:40:39 +0700

    Update: change wrong data type from {} to int, and move orrect payload test to above

--- src/Domains/threads/entities/_test/PostedThread.test.js
+++ src/Domains/threads/entities/_test/PostedThread.test.js
@@ -1,30 +1,6 @@
 const PostedThread = require('../PostedThread');
 
 describe('a PostedThread entities', () => {
-    it('should throw error when payload did not contain needed property', () => {
-        // Arrange
-        const payload = {
-            // Missing date property
-            title: 'stringTitle',
-            owner: 'stringOwnerId',
-        };
-
-        // Action and Assert
-        expect(() => new PostedThread(payload)).toThrowError('POSTED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
-    });
-
-    it('should throw error when payload properties are not of expected type', () => {
-        // Arrange
-        const payload = {
-            id: {}, // Wrong data type
-            title: 'stringTitle',
-            owner: 'stringOwnerId',
-        };
-
-        // Action and Assert
-        expect(() => new PostedThread(payload)).toThrowError('POSTED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    });
-    
     it('should create PostedThread object correctly', () => {
         // Arrange
         const payload = {
@@ -41,4 +17,29 @@ describe('a PostedThread entities', () => {
         expect(title).toEqual(payload.title);
         expect(owner).toEqual(payload.owner);
     });
+
+    it('should throw error when payload did not contain needed property', () => {
+        // Arrange
+        const payload = {
+            // Missing date property
+            title: 'stringTitle',
+            owner: 'stringOwnerId',
+        };
+
+        // Action and Assert
+        expect(() => new PostedThread(payload)).toThrowError('POSTED_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload properties are not of expected type', () => {
+        // Arrange
+        const payload = {
+            id: 123, // Wrong data type
+            title: 'stringTitle',
+            owner: 'stringOwnerId',
+        };
+
+        // Action and Assert
+        expect(() => new PostedThread(payload)).toThrowError('POSTED_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+    
 });
