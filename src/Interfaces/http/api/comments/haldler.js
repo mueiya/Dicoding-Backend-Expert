40709d0964ba7025c1c1795e2ebd98ac939c864f
mueiya <mueiya.whatever@gmail.com>
2024-04-24 12:37:50 +0700

    Update: add postCommentHandler function to CommentHandler class

--- src/Interfaces/http/api/comments/haldler.js
+++ src/Interfaces/http/api/comments/haldler.js
@@ -1,6 +1,31 @@
+const PostCommentUseCase = require('../../../../Applications/use_case/PostCommentUseCase');
+
 class CommentHandler {
   constructor(container) {
     this._container = container;
+  }
+
+  async postCommentHandler(request, h) {
+    const { payload } = request;
+    const { id: userId } = request.auth.credentials;
+    const useCasePayload = {
+      content: payload.content,
+      owner: userId,
+    };
+
+    const postCommentUseCase = this._container.getInstance(
+      PostCommentUseCase.name,
+    );
+    const postedComment = await postCommentUseCase.execute(useCasePayload);
+    const response = h.response({
+      status: 'success',
+      message: 'Comment posted successfully',
+      data: {
+        addedCommment: postedComment,
+      },
+    });
+    response.code(201);
+    return response;
   }
 }
 
